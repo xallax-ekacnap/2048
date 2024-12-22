de09a9cf30ae9e49663b989b235eb466e5fb41db
Aaron Hoffman <aaron_hoffman@fastmail.com>
2024-12-21 19:57:52 -0800

    updated code to work with streamlit and (hopefully) more rows/columns

--- 2048_nuke.py
+++ 2048_nuke.py
@@ -1,59 +1,30 @@
 import random
 import sys
+import streamlit as st
 
-try:
-    import tty, termios
-except ImportError:
-    # Probably Windows.
-    try:
-        import msvcrt
-    except ImportError:
-        # FIXME what to do on other platforms?
-        # Just give up here.
-        raise ImportError('getch not available')
-    else:
-        getch = msvcrt.getch
-else:
-    def getch():
-        """getch() -> key character
-
-        Read a single keypress from stdin and return the resulting character. 
-        Nothing is echoed to the console. This call will block if a keypress 
-        is not already available, but will not wait for Enter to be pressed. 
-
-        If the pressed key was a modifier key, nothing will be detected; if
-        it were a special function key, it may return the first character of
-        of an escape sequence, leaving additional characters in the buffer.
-        """
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
+from streamlit_shortcuts import add_keyboard_shortcuts
+from streamlit_extras.stylable_container import stylable_container
     
 def add_new_value(board, r=None):
-    if not r:
-        r = random.randint(0, 9)
-    if r == 0:
-        index = (random.randint(0, 3), random.randint(0, 3))
-        if board[index[0]][index[1]] == 0:
-            board[index[0]][index[1]] = Cell(4)
-        else:
-            add_new_value(board, r)
+    spots = []
+    for i, row in enumerate(board):
+        for j, item in enumerate(row):
+            if item == 0:
+                spots.append((i, j))
+    if len(spots) == 0:
+        return board
+    index = random.choice(spots)
+    if random.randint(0, 9) == 0:
+        board[index[0]][index[1]] = Cell(4)
     else:
-        index = (random.randint(0, 3), random.randint(0, 3))
-        if board[index[0]][index[1]] == 0:
-            board[index[0]][index[1]] = Cell(2)
-        else:
-            add_new_value(board, r)
+        board[index[0]][index[1]] = Cell(2)
+    return board
+        
 
 # check, rest = row[0], row[1:]
 
 def move_row_left(row):
-    for i in range(1, 4):
+    for i in range(1, len(row)):
         for j in reversed(range(i)):
             row[j], row[j + 1] = check_pair(row[j], row[j + 1])
     for item in row:
@@ -62,7 +33,7 @@ def move_row_left(row):
 
 
 def move_board_left(board):
-    for i in range(4):
+    for i in range(len(board)):
         board[i] = move_row_left(board[i])
     return board
 
@@ -72,40 +43,42 @@ def move_board_right(board):
     return board
 
 def move_board_up(board):
-    new_board = [[], [], [], []]
-    for i in range(4):
-        for j in range(4):
-            new_board[i].append(board[j][i])
+    new_board = [[0 for _ in range(len(board[0]))] for _ in range(len(board))]
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            new_board[i][j] = board[j][i]
     new_board = move_board_left(new_board)
-    for i in range(4):
-        for j in range(4):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
             board[j][i] = new_board[i][j]
     return board
 
 def move_board_down(board):
-    new_board = [[], [], [], []]
-    for i in range(4):
-        for j in range(4):
-            new_board[i].append(board[j][i])
+    new_board = [[0 for _ in range(len(board[0]))] for _ in range(len(board))]
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            new_board[i][j] = board[j][i]
     new_board = move_board_right(new_board)
-    for i in range(4):
-        for j in range(4):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
             board[j][i] = new_board[i][j]
     return board
 def check_pair(a, b):
     if a == b and not a.combined and not b.combined and a != 0: #combining
-        return Cell(a * 2, True), Cell(0) 
+        new_value = a * 2
+        st.session_state.score += new_value
+        return Cell(new_value, True), Cell(0) 
     if a == 0 and b != 0: #moving left
         return b, Cell(0)
     return a, b
             
 
-def display(board):
-    for i in range(4):
-        for j in range(4):
-            print(board[i][j], end=' ')
-        print()
-    print()
+# def display(board):
+#     for i in range(4):
+#         for j in range(4):
+#             print(board[i][j], end=' ')
+#         print()
+#     print()
 
 class Cell(int):
     def __new__(cls, value, *args, **kwargs):
@@ -120,29 +93,120 @@ class Cell(int):
     def __str__(self):
         return f"{self.value}"
 
+def decide_direction(name):
+    print("before movement")
+    # display(st.session_state.board)
+    
+    if name == 'Left':
+        st.session_state.board = move_board_left(st.session_state.board)
+    if name == 'Right':
+        st.session_state.board = move_board_right(st.session_state.board)
+    if name == 'Up':
+        st.session_state.board = move_board_up(st.session_state.board)
+    if name == 'Down':
+        st.session_state.board = move_board_down(st.session_state.board)
+    
+    print("after movement")
+    # display(st.session_state.board)
+    st.session_state.board = add_new_value(st.session_state.board)
+    
+    print("after adding new value")
+    # display(st.session_state.board)
+
 
 # board = [[Cell(0) for i in range(4)] for j in range(4)]
 
-board = [[Cell(0), Cell(2), Cell(2), Cell(4)], [Cell(0), Cell(2), Cell(2), Cell(0)], [Cell(2), Cell(2), Cell(2), Cell(4)], [Cell(8), Cell(2), Cell(4), Cell(4)]]
+edge_num = 4
 
-while True:
-    keyboard = getch()
-    if keyboard == 'w':
-        board = move_board_up(board)
-    elif keyboard == 'a':
-        board = move_board_left(board)
-    elif keyboard == 's':
-        board = move_board_down(board)
-    elif keyboard == 'd':
-        board = move_board_right(board)
-    elif keyboard == 'q':
-        break
 
-    add_new_value(board)
-    display(board)
+board_cont = st.container(border=False)
 
-# assert move_row_left([0, 0, 0, 0]) == [0, 0, 0, 0]
-# assert move_row_left([0, 0, 0, 2]) == [2, 0, 0, 0]
-# assert move_row_left([2, 2, 4, 0]) == [4, 4, 0, 0]
-# assert move_row_left([8, 4, 2, 2]) == [8, 4, 4, 0]
-# assert move_row_left([2, 2, 2, 2]) == [4, 4, 0, 0]
+if "score" not in st.session_state:
+    st.session_state.score = 0
+
+with st.container():
+    st.markdown('<div class="board-container"></div>', unsafe_allow_html=True)
+
+
+
+col1, col2, col3, col4 = st.columns(4)
+names = ['Left', 'Right','Up', 'Down']
+arrows = ['←', '→', '↑','↓']
+
+# for i, col in enumerate([col1, col2, col3, col4]):
+#     with col:
+#         st.button(label=arrows[i], key=names[i], on_click=decide_direction(names[i]))
+
+with st.sidebar:
+
+    st.markdown(f"<h1 style='text-align: center;'>Score: {st.session_state.score}</h1>", unsafe_allow_html=True)
+
+for i, col in enumerate([col1, col2, col3, col4]):
+    with col:
+        if st.button(label=arrows[i], key=names[i]):
+            decide_direction(names[i])
+
+add_keyboard_shortcuts({
+    'A': "←",
+    'D': "→",
+    'W': "↑",
+    'S': "↓"
+})
+
+#colors in hex values on a gradient
+colors = {
+0: "#b3d1e6",  # Light blue
+    2: "#9fc6e0",  # Soft blue
+    4: "#8bbbd9",  # Cool blue
+    8: "#77b0d2",  # Aqua blue
+    16: "#63a5cb", # Cyan
+    32: "#4f99c4", # Blue-green
+    64: "#3b8ebd", # Teal
+    128: "#2a84a3",# Deep teal
+    256: "#1b7990",# Sea green
+    512: "#0e6f7e",# Forest green
+    1024: "#066566",# Dark teal
+    2048: "#00514d" # Deep green
+}
+
+
+
+if "board" not in st.session_state:
+    board = [[Cell(0) for i in range(edge_num)] for j in range(edge_num)]
+    board = add_new_value(board)
+    st.session_state.board = board
+
+cols = board_cont.columns(edge_num, gap="small")
+# for i, col in enumerate(cols):
+#     with col:
+#         for j in range(4):
+#             var = st.session_state.board[j][i]
+#             with stylable_container(
+#                 key="box",
+#                 css_styles=" {background-color: grey; max-width: 100px;}"
+#                 ):
+#                 box = st.container(height=100,border=True)
+            
+#             box.write(f'<span style="color: {colors[var]}; font-size: 2em;">{str(var)}</span>', unsafe_allow_html=True)
+for i, col in enumerate(cols):
+    with col:
+        for j in range(edge_num):
+            var = st.session_state.board[j][i]
+            color = colors[var]
+            st.write(
+                f"""
+                <div style="
+                    height: {680 / edge_num}px; 
+                    width: {680 / edge_num}px; 
+                    background-color: {color}; 
+                    display: flex; 
+                    align-items: center; 
+                    justify-content: center; 
+                    border-radius: 10px;
+                    margin: 5px;
+                    ">
+                    <span style="color: white; font-size: 2em;">{str(var) if var != 0 else ''}</span>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
